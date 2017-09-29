2b2a65d793e695c1f3b23d2ca47644c11e87a559
Steven Halase <steven.halase@gmail.com>
2017-09-28 23:29:13 -0500

    Added tab section

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $('.information-content .item').each(function(index) {
-    $(this).click(function(e) {
-      $(e.target.previousElementSibling).toggleClass('open-text')
-      e.target.dataset.item == '+' ? e.target.dataset.item = '-' : e.target.dataset.item = '+';
-    })
-  })
+  // $('.information-content .item').each(function(index) {
+  //   $(this).click(function(e) {
+  //     $(e.target.previousElementSibling).toggleClass('open-text')
+  //     e.target.dataset.item == '+' ? e.target.dataset.item = '-' : e.target.dataset.item = '+';
+  //   })
+  // })
 
   $('.process-content .item').each(function(index) {
     $(this).click(function(e) {
@@ -13,6 +13,21 @@ $(document).ready(function() {
       })
       $(e.target).toggleClass('process-selected')
       $('.image-part').css('background-image', 'url(../images/'+$(e.target).data().image+'.png)')
+    })
+  })
+
+  $('.tab-headers div').each(function(index) {
+    $(this).click(function(e) {
+      $('[data-tab-for]').each(function(index) {
+        $(this).removeClass('tab--selected')
+      })
+      $('[data-tab-header]').each(function(index) {
+        $(this).removeClass('tab-header--selected');
+      })
+
+      var tab = e.target.dataset.tabHeader;
+      $(e.target).toggleClass('tab-header--selected');
+      $('[data-tab-for='+tab+']').toggleClass('tab--selected');
     })
   })
 })
